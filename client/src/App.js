b614dc1f714426396c62322b84c8a15c2a471037
Kim C <kimkly.chau@gmail.com>
2019-08-05 12:40:03 -0700

    added PostCollectCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import UploadImg from "./components/UploadImg";
+import Wrapper from "./components/Wrapper";
+import PostCollectCard from "./components/PostCollectCard";
+
 
 function App() {
   return (
     <div>
       <UploadImg/>
+      <Wrapper>
+        <PostCollectCard/>
+      </Wrapper>
     </div>
   );
 }
