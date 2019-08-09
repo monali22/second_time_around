a97557d5b9359d2dcae86686ce99377ad9b4bb4b
Kim C <kimkly.chau@gmail.com>
2019-08-09 11:14:02 -0700

    update

--- client/src/components/SimpleMap/index.js
+++ client/src/components/SimpleMap/index.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import { log } from 'util';
 
 const style = {
     width: '90%',
     height: '70%',
     position: 'relative'
 }
+
 class SimpleMap extends Component {
     state = {
-        
+
     }
 
-
- componentDidMount  () {
+    componentDidMount() {
         navigator.geolocation.getCurrentPosition((position) => {
             console.log("Trying");
             console.log(position)
-            
+
             this.setState({
                 position
             })
@@ -28,32 +27,30 @@ class SimpleMap extends Component {
         })
     }
 
-   
     render() {
         return (
             <div>
-
-            {this.state.position?
-            (<Map
-            google={this.props.google}
-            style={style}
-            initialCenter={{
-                lat: this.state.position.coords.latitude,
-                lng: this.state.position.coords.longitude
-            }}
-            zoom={15}
-            onClick={this.onMapClicked}
-            >
-                <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    {/* <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-                </div> */}
-                </InfoWindow>
-            </Map >):(
-                <p>Map not ready</p>
-            )}
+                {this.state.position ?
+                    (<Map
+                        google={this.props.google}
+                        style={style}
+                        initialCenter={{
+                            lat: this.state.position.coords.latitude,
+                            lng: this.state.position.coords.longitude
+                        }}
+                        zoom={15}
+                        onClick={this.onMapClicked}
+                    >
+                        <Marker onClick={this.onMarkerClick}
+                            name={'Current location'} />
+                        <InfoWindow onClose={this.onInfoWindowClose}>
+                            {/* <div>
+                                <h1>{this.state.selectedPlace.name}</h1>
+                                </div> */}
+                        </InfoWindow>
+                    </Map >) : (
+                        <p>Map not ready</p>
+                    )}
             </div>
         );
     }
