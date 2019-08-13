442c8e5b5fc4acc674f8f5a97dd84eef16f5bf8a
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-08-13 12:11:08 -0700

    added localhost to api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,43 +2,43 @@ import axios from "axios";
 
 export default{
     savePost: function(postdata){
-      return axios.post("/api/stock",postdata);
+      return axios.post("http://localhost:3001/api/stock",postdata);
     },
     getDataById: function(id){
-      return axios.get("/api/stock/" + id);
+      return axios.get("http://localhost:3001/api/stock/" + id);
     },
     getData: function(){
-      return axios.get("/api/stock/");
+      return axios.get("http://localhost:3001/api/stock/");
     },
     deleteData: function(id) {
-      return axios.delete("/api/stock/" + id);
+      return axios.delete("http://localhost:3001/api/stock/" + id);
     },
     updatePost:function(id){
-      return axios.post("/api/stock/"+id);
+      return axios.post("http://localhost:3001/api/stock/"+id);
     },
 // request the information to send it to the server
     saveItem: function(item) {
-      return axios.post("/api/freeItems", item);
+      return axios.post("http://localhost:3001/api/freeItems", item);
     },
     getEmail:function(msg){
-      return axios.post("/api/freeItems",msg);
+      return axios.post("http://localhost:3001/api/freeItems",msg);
     },
     saveUser : function(userData){
-      return axios.post("/api/user",userData);
+      return axios.post("http://localhost:3001/api/user",userData);
     },
     updateUserData:function(name,data){
-      return axios.put("/api/user/:"+name,{data});
+      return axios.put("http://localhost:3001/api/user/:"+name,{data});
     },
     findUser:function(email){
-      return axios.get("/api/user/?email=" + email);
+      return axios.get("http://localhost:3001/api/user/?email=" + email);
     },
     getUData: function(){
-      return axios.get("/api/user/");
+      return axios.get("http://localhost:3001/api/user/");
     },
     findUserID:function(id){
-      return axios.get("/api/user/" + id);
+      return axios.get("http://localhost:3001/api/user/" + id);
     },
     getUSerData: function(id){
-      return axios.get("/api/stock/?user_id="+id);
+      return axios.get("http://localhost:3001/api/stock/?user_id="+id);
     },
 };
